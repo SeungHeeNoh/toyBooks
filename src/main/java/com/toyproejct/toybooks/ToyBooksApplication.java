76ff940cf0f48dd6093f400cfe17e09dc743fdcc
nsh <nsh2398@naver.com>
2022-03-14 14:33:39 +0900

    initialize toyBooks project

---
+++ src/main/java/com/toyproejct/toybooks/ToyBooksApplication.java
@@ -0,0 +1,13 @@
+package com.toyproejct.toybooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyBooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toyproejct/toybooks/ToyBooksApplicationTests.java
@@ -0,0 +1,14 @@
+package com.toyproejct.toybooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
